86e0f03a3bc61f6a2ea600d4c5a8930e2a1aa1a6
donecho.20pt@gmail.com <donecho.20pt@gmail.com>
2025-12-13 23:00:24 +0630

    3.2-useState Hook

--- spa-react/src/App.js
+++ spa-react/src/App.js
@@ -1,27 +1,22 @@
 // import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 import QAImage from './assets/image/image_1.png';
+
 
 function App() {
 
-  let name = "Zin Phyo Thant";
+  // let name = "Zin Phyo Thant";
+  let [name,setName]=useState("Zin Phyo Thant"); //[getElementError,setter]
+  
   let changeName= ()=>{
-    name = "Aung Aung";
+    setName("Aung Aung");
     console.log(name);
   };
   
   return (
     <div className='app'>
       <h1>Hello World {name}</h1>
-      {/**1st wway - public folder way */}
-      {/**absolute path */}
-      {/* <img src="http://localhost:3000/image_1.png" alt="" /> */}
-      {/**relative path */}
-      {/* <img src="/image_1.png" alt="" /> */}
-
-      {/**2nd way - import */}
-      {/* <img src={QAImage} alt=''/> */}
-
       <button onClick={changeName}>Click to change name</button>
 
 
