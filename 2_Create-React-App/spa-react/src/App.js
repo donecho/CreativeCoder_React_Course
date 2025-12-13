43d52480f6f15d4b81d6d3469322fb898eb6539d
donecho.20pt@gmail.com <donecho.20pt@gmail.com>
2025-12-13 22:43:56 +0630

    2_Create-React-App

--- 2_Create-React-App/spa-react/src/App.js
+++ 2_Create-React-App/spa-react/src/App.js
@@ -1,26 +1,17 @@
 // import logo from './logo.svg';
 import './App.css';
-import QAImage from './assets/image/image_1.png';
 
 function App() {
   return (
-    <div className='app'>
+    <div>
       <h1>Hello World</h1>
-      {/**1st wway - public folder way */}
-      {/**absolute path */}
-      {/* <img src="http://localhost:3000/image_1.png" alt="" /> */}
-      {/**relative path */}
-      {/* <img src="/image_1.png" alt="" /> */}
 
-      {/**2nd way - import */}
-      {/* <img src={QAImage} alt=''/> */}
+      {/** 1st way - public folder way */}
+
+      {/**absolute path */}
+      <imgsrc
     </div>
   );
 }
 
 export default App;
-
-
-//npm run script
-
-
