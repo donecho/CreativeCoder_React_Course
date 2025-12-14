bf4a61fe5a0486aa430152573588542685fea160
donecho.20pt@gmail.com <donecho.20pt@gmail.com>
2025-12-14 12:09:50 +0630

    14-state-update-detail

--- spa-react/src/App.js
+++ spa-react/src/App.js
@@ -1,44 +1,19 @@
-// import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 import { useState } from 'react';
-import QAImage from './assets/image/image_1.png';
+export default function App() {
 
+  let [count,setCount] = useState(0);
 
-function App() {
+  let increment = () => {
+    setCount((prevState)=>prevState+1)
+  }
 
-  // let name = "Zin Phyo Thant";
-  let [name,setName]=useState("Zin Phyo Thant"); //[getElementError,setter]
-  
-  let changeName= ()=>{
-    setName("Aung Aung");
-    console.log(name);
-  };
-
-  //working with list
-  let [post,setPost]=useState([
-    {id:1,title:"Post 1"},
-    {id:2,title:"Post 2"},
-    {id:3,title:"Post 3"},
-  ]);
-  
   return (
-    <div className='app'>
-      <h1>Hello World {name}</h1>
-      <button onClick={changeName}>Click to change name</button>
-
-      <h1>List of Posts</h1>
-      <ul>
-        {post.map((post)=>(
-          <li key={post.id}>{post.title}</li>
-        ))}
-      </ul>
+    <div>
+      <h1>Counter</h1>
+      <h3>Count - {count}</h3>
+      <button onClick={increment}>increments</button>
+    
     </div>
-  );
+  )
 }
-
-export default App;
-
-
-//npm run script
-
-
