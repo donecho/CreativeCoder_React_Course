543d53db8aed61507fd8983ec1a0321c0fec3486
donecho.20pt@gmail.com <donecho.20pt@gmail.com>
2025-12-14 18:53:00 +0630

    21-Portal

--- spa-react/src/components/Modal/index.js
+++ spa-react/src/components/Modal/index.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import './index.css'
+import ReactDom from 'react-dom';
+
 
 export default function Modal({children}) {
   return (
+    ReactDom.createPortal(
     <div className='modal-backdrop'>
       <div className="modal">
         {children}
       </div>
     </div>
+    ),document.getElementById('modal')
   )
 }
 
