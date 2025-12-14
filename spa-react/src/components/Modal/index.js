838c574065458b1c67f9a24d1a9e786a9db3cbd7
donecho.20pt@gmail.com <donecho.20pt@gmail.com>
2025-12-14 20:52:38 +0630

    24-dynamic-claaname-and-css-module

--- spa-react/src/components/Modal/index.js
+++ spa-react/src/components/Modal/index.js
@@ -4,15 +4,12 @@ import ReactDom from 'react-dom';
 
 
 export default function Modal({children,danger}) {
+  let className = danger ? 'border-red':'border-blue';
   return (
     ReactDom.createPortal(
     <div className='modal-container'>
       <div className='modal-backdrop'>
-        <div className="modal" style={{ 
-          border: '4px solid',
-          borderColor: danger ? 'red':'blue'
-
-         }}>
+        <div className={`modal ${className}`}>
           {children}
         </div>
       </div>
