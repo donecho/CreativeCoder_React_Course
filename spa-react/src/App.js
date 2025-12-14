6fd5da68185f149a881707ce928e3536543c4893
donecho.20pt@gmail.com <donecho.20pt@gmail.com>
2025-12-14 18:38:44 +0630

    20-pass-function-as-props

--- spa-react/src/App.js
+++ spa-react/src/App.js
@@ -6,8 +6,6 @@ import PostsList from './components/PostList';
 import Modal from './components/Modal';
 
 function App() {
-
-
   //working with list
   let [posts,setPost]=useState([
     {id:1,title:"Post 1"},
@@ -15,17 +13,25 @@ function App() {
     {id:3,title:"Post 3"},
   ]);
 
+  let [showModal,setShowModal] = useState(false);
+
 
   
   return (
     <>
-      <Navbar/>
+      <Navbar setShowModal={setShowModal}/>
       <PostsList posts={posts}/>
-      <Modal>
+      {/* <Modal> */}
         {/* modal content */}
-        <h1>Zoom class is available now.</h1>
-        <p>feel free to <a href=''>Join</a> here </p>
-      </Modal>
+        {/* <h1>Zoom class is available now.</h1>
+        <p>feel free to <a href='www.youtube.com'>Join</a> here </p> */}
+      {/* </Modal> */}
+
+    {showModal &&  <Modal>
+        <h1>Term and Condition</h1>
+        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque distinctio repudiandae dolorum laudantium veniam nihil minima quo doloremque sapiente excepturi tempora culpa in, illum eum voluptas, quod ipsam! Quibusdam, incidunt. </p>
+        <button onClick={()=>setShowModal(false)}>Close</button>
+      </Modal>}
     </>  //it called react fragment
   );
 }
