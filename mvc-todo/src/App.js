63de2dd07f448591ec7377162e4378cb85775d84
donecho.20pt@gmail.com <donecho.20pt@gmail.com>
2025-12-21 02:26:50 +0630

    40-Fetch-All-Api

--- mvc-todo/src/App.js
+++ mvc-todo/src/App.js
@@ -5,26 +5,33 @@ import TodoList from './components/TodoList.js'
 import CheckAllAndRemaining from './components/CheckAllAndRemaining.js'
 import TodoFilters from './components/TodoFilters.js'
 import ClearCompletedBtn from './components/ClearCompletedBtn.js'
+import { useEffect, useState } from 'react';
 
 
 function App() {
+
+  let [todos,setTodos] = useState([]);
+
+  //Api
+  useEffect(()=>{
+    fetch('http://localhost:3001/todos')
+    .then(res => res.json())
+    .then((todos)=>{
+      setTodos(todos);
+    })
+  },[])
+
   return (
     <div className="todo-app-container">
       <div className="todo-app">
         <h2>Todo App</h2>
 
         <TodoForm/>
-    
-        <TodoList/>
-       
-       <CheckAllAndRemaining/>
-        
+        <TodoList todos={todos}/> 
+        <CheckAllAndRemaining/>
         <div className="other-buttons-container">
         <TodoFilters/>
-        
-
           <ClearCompletedBtn/>
-        
         </div>
       </div>
     </div>
